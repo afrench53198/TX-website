9e14bc4323d49ed1176a3d62072e4fa7e30f353b
Adam French <Afrench53198@gmail.com>
2018-03-13 17:04:31 -0700

    Simple form validation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-document.readyState()
+// Uses querySelector so enter a string for each parameter. If getting form by id the parameter would be entered as "#formid"
+var FormValidator = function (form,emailField,passwordField) {
+    this.form = document.querySelector(form);
+    this.email = document.querySelector(emailField);
+    this.password = document.querySelector(passwordField); 
+
+    this.validEmail = function() {
+        // validate email and add error class if not valid
+    }
+    
+    this.validPassword = function() {
+        // validate password and add error class if not valid
+    }
+    this.validateForm = function() {
+        // validate form using previous two functions and if valid send asychronous post request 
+    }
+
+}
+
+function validateLgnForm () {
+    var pswrd = document.querySelector("#pswrd");
+    var pswrdCnfrm = document.querySelector("#pswrdcnfrm");
+    if (pswrdCnfrm) {
+        var match = (pswrd.value == pswrdCnfrm.value);
+        if (!match) {
+            window.alert("Your passwords must match!"); 
+        }
+    }
+}
