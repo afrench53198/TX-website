997da230c92bb5d0cd449c5c56ca914da707223d
Adam French <Afrench53198@gmail.com>
2018-03-16 15:33:47 -0700

    Cleaned up form validation

--- js/index.js
+++ js/index.js
@@ -1,34 +1,30 @@
-// Uses querySelector so enter a string for each parameter. If getting form by id the parameter would be entered as "#formid"
-var FormValidator = function (form,emailField,passwordField) {
-    this.form = document.querySelector(form);
-    this.email = document.querySelector(emailField);
-    this.password = document.querySelector(passwordField); 
 
-    this.validEmail = function() {
-        // validate email and add error class if not valid
-    }
-    
-    this.validPassword = function() {
-        // validate password and add error class if not valid
-    }
-    this.validateForm = function() {
-        // validate form using previous two functions and if valid send asychronous post request 
-    }
-}
 
- function validateForm () {
+window.onload = function () {
+// Selectors 
     var form = document.querySelector("form");
-    var formData = new FormData(form);
-    console.log(formData.values);
     var pswrd = document.querySelector("#pswrd");
     var pswrdCnfrm = document.querySelector("#pswrdcnfrm");
-    if (pswrdCnfrm) {
-        var match = (pswrd.value == pswrdCnfrm.value);
-        if (!match) {
-            var pswrdLabel = document.querySelector("#pswrdlbl") 
+    var submitButton = document.querySelector("form").querySelector(".grdnt-btn");
+    var pswrdLabel = document.querySelector("#pswrdlbl"), pswrdCnfrmLabel = document.querySelector("#pswrdcnfrmlbl");
+
+//Functions
+    function validateForm (e) {
+        if (pswrdCnfrm) {
+            var match = (pswrd.value == pswrdCnfrm.value);
+            if (!match) {
+                pswrdLabel.textContent = "Passwords Need to match!";
+                pswrdCnfrmLabel.textContent = "Passwords Need to match!";
+                pswrdCnfrm.classList.add("error");
+                pswrd.classList.add("error");
+                e.preventDefault();
+            }
         }
     }
+// Call functions
+    submitButton.addEventListener("click",validateForm,false);
 }
+
 
 
 
